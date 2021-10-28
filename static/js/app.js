6d5e2aa2dd2d2eb7ec80158ead02aacffe8815ad
Mateusz Szczesniak <mszczesniak@live.com>
2021-10-28 15:18:17 +0200

    UPDATE-28OCT-toc-checker-added-tweaks

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 
 const ot_prompt = document.querySelector("#ot_prompt");
-const date = document.querySelector("#date");
+const date = document.querySelector("#id_date");
 const start_hour = document.querySelector("#start_hour");
 const end_hour = document.querySelector("#end_hour");
-let date = 0;
+let date_value = 0;
 let start = 0;
 let end = 0;
 const working_hours_context = document.querySelector("#working_hours").value;
@@ -19,9 +19,16 @@ if (working_hours_context === '1') {
 }
 
 date.addEventListener('blur', function (event) {
-      date = date.value;
-      start_hour.value = date;
-      end_hour.value = date;
+      date_value = date.value;
+      console.log(date_value);
+      let temp_start = new Date(date_value);
+      let temp_end = new Date (date_value);
+      temp_start.setHours(8);
+      temp_end.setHours(20);
+      console.log(temp_start);
+      console.log(temp_start.toISOString().substring(0, 16));
+      start_hour.value = temp_start.toISOString().substring(0, 16);
+      end_hour.value = temp_end.toISOString().substring(0, 16);
 });
 
 
