c8f98eafd47c9ac738c79d58759291b2b0231a6c
Mateusz Szczesniak <mszczesniak@live.com>
2021-11-01 16:38:33 +0100

    UPDATE-01NOV-register-captcha-v3-handling-config-update

--- FILM_CREW_HELPER/settings.py
+++ FILM_CREW_HELPER/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
-from boto.s3.connection import S3Connection
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
